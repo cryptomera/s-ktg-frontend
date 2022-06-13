a08fd525185a9e10899d464eed6e4c3eb7a72474
cryptowarrier <cryptomusha@gmail.com>
2022-06-13 13:35:55 +0900

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import landing from '../assets/images/landing.png';
-
+import '../custom.css';
 const Home = () => {
+  const [showText, setShowText] = useState(true);
+  const [showLogger, setShowLogger] = useState();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowText();
+    }, 6000);
+    return () => clearTimeout(timer);
+  });
+  useEffect(() => {
+    let timer;
+    if(!showText) {
+      timer = setTimeout(() => {
+        setShowLogger(true);
+      }, 3000);
+    }
+    return () => clearTimeout(timer);
+  }, [showText])
   return (
-    <div className="flex justify-center">
-      <img className="xl:w-3/4 sm:w-full" src={landing}/>
+    <div className='landing-container'>
+      <div className='wrapper'>
+        {
+          showText && (
+            <ul className='dynamic-txts'>
+              <li>
+                <span>MOMENT</span>
+              </li>
+              <li>
+                <span>NOW</span>
+              </li>
+              <li>
+                <span>US</span>
+              </li>
+            </ul>
+          )
+        }
+        {
+          showLogger && (
+            <img src={landing}/>
+          )
+        }
+
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // animation: {
+      //   'typing': 'typing 1.5s steps(5) infinite',
+      //   'slide': 'slide 6s steps(3) infinite'
+      // },
+      // keyframes: {
+      //   'typing': {
+      //     '100%': {
+      //       left: '100%',
+      //       margin: '0 -35px 0 35px'
+      //     }
+      //   },
+      //   'slide': {
+      //     '100%': {
+      //       top: '-300px'
+      //     }
+      //   }
+      // }
+    },
   },
   plugins: [],
 }
